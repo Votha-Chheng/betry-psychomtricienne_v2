b342096271777ebe3ef3d56b76e05946692174e3
Votha-Chheng <votha_chheng@gmx.fr>
2024-03-09 21:42:12 +0100

    Correction de BigWords

--- src/components/homeComponents/whoAmI/BigWords.tsx
+++ src/components/homeComponents/whoAmI/BigWords.tsx
@@ -1,98 +1,18 @@
-import { inter } from '@/fonts/inter'
-import { useScrollEdFromElement } from '@/hooks/useScrollTop'
-import React, { FC, RefObject, useEffect, useRef } from 'react'
+import React, { FC } from 'react'
+import WordTag from './WordTag'
 
 const BigWords: FC = () => {
 
-  const bigWordsRef1=useRef<HTMLDivElement>(null)
-  const bigWordsRef2=useRef<HTMLDivElement>(null)
-  const bigWordsRef3=useRef<HTMLDivElement>(null)
-  const bigWordsRef4=useRef<HTMLDivElement>(null)
-  const bigWordsRef5=useRef<HTMLDivElement>(null)
-  const bigWordsRef6=useRef<HTMLDivElement>(null)
-  const bigWordsRef7=useRef<HTMLDivElement>(null)
-  const bigWordsRef8=useRef<HTMLDivElement>(null)
-
-  const firstWordScrolled = useScrollEdFromElement(bigWordsRef1)
-  const secondWordScrolled = useScrollEdFromElement(bigWordsRef2)
-  const thirdWordScrolled = useScrollEdFromElement(bigWordsRef3)
-  const fourthWordScrolled = useScrollEdFromElement(bigWordsRef4)
-  const fifthWordScrolled = useScrollEdFromElement(bigWordsRef5)
-  const sixthWordScrolled = useScrollEdFromElement(bigWordsRef6)
-  const seventhWordScrolled = useScrollEdFromElement(bigWordsRef7)
-  const eightWordScrolled = useScrollEdFromElement(bigWordsRef8)
-
-
-  const getPercentage = (scrolledElement: number, left : boolean)=> {
-    if(scrolledElement<0){
-      return left ? -100 : 100
-
-    } else if((scrolledElement>0) && (scrolledElement < 500)){
-      return left? -((500-scrolledElement)/500)*100 : ((500 - scrolledElement)/500)*100
-
-    } else if((scrolledElement >500)){
-      return 0
-    }
-  }
-
-  const words = [
-    {
-      word: "épanouissement",
-      ref: bigWordsRef1,
-      scrolled: firstWordScrolled
-    },
-    {
-      word: "connaissance de soi",
-      ref: bigWordsRef2,
-      scrolled: secondWordScrolled
-    },
-    {
-      word: "méditation",
-      ref: bigWordsRef3,
-      scrolled: thirdWordScrolled
-    },
-    {
-      word: "relations aux autres",
-      ref: bigWordsRef4,
-      scrolled: fourthWordScrolled
-    },
-    {
-      word: "équilibre",
-      ref: bigWordsRef5,
-      scrolled: fifthWordScrolled
-    },
-    {
-      word: "gestion des émotions",
-      ref: bigWordsRef6,
-      scrolled: sixthWordScrolled
-    },
-    {
-      word: "relaxation",
-      ref: bigWordsRef7,
-      scrolled: seventhWordScrolled
-    },
-    {
-      word: "potentialité",
-      ref: bigWordsRef8,
-      scrolled: eightWordScrolled
-    }
-  ]
+  const words = ["épanouissement", "connaissance de soi", "méditation", "relations aux autres", "équilibre", "gestion des émotions", "relaxation", "potentialité" ]
 
   return (
     <section className={`px-7 py-[450px] overflow-hidden`} >
       {
-        words.map((element: {word: string, ref: RefObject<HTMLDivElement>, scrolled: number}, index: number)=> (
-          <div 
-            key={index}
-            className={"flex justify-center mb-5 md:mb-12"}
-            style={{transform:`translateX(${getPercentage(element.scrolled, index%2 === 0 ? true: false )}%)`}}
-          >
-          <div className='absolute -top-[100vh]' ref={element.ref}/>
-          <span className={`${inter.className} inline-block bg-anti-flash-white p-4 text-xl sm:text-3xl md:text-5xl xl:text-8xl text-charcoal uppercase transition-transform`}>
-            {element.word}
-          </span>
-        </div>
-        ))
+        words.map((word: string, index: number)=> (
+          <div>
+            <WordTag index={index} word={word} />
+          </div>
+         ))
       }
     </section>
   )
---
+++ src/components/homeComponents/whoAmI/WordTag.tsx
@@ -0,0 +1,57 @@
+import { inter } from '@/fonts/inter'
+import { useScrollY } from '@/hooks/useScrollTop'
+import React, { FC, useEffect, useMemo, useRef, useState } from 'react'
+
+type WordTagProps = {
+  word: string
+  index: number
+}
+
+const WordTag: FC<WordTagProps> = ({word, index}) => {
+  const [positionY, setPositionY] = useState<number>(0)
+
+  const wordRef = useRef<HTMLDivElement>(null)
+  const Y = useScrollY()
+
+  useEffect(()=> {
+    if(wordRef.current){
+      setPositionY(wordRef.current.getBoundingClientRect().top)
+    }
+  }, [])
+
+
+  const getPercentage = (scrollY: number, positionY: number, left : boolean)=> {
+    let scrolledElement = scrollY-(positionY)
+
+    if(scrolledElement<0){
+      return left ? -100 : 100
+
+    } else if((scrolledElement>0) && (scrolledElement < 500)){
+      return left? -((500-scrolledElement)/500)*100 : ((500 - scrolledElement)/500)*100
+
+    } else if((scrolledElement >500)){
+      return 0
+    }
+    
+    return -100
+  }
+
+  const translation = useMemo(()=> {
+    return getPercentage(Y, positionY, index%2 === 0 ? true: false )
+  }, [positionY, Y])
+  
+  return (
+    <div  
+      ref={wordRef}
+      className={"flex justify-center mb-5 md:mb-12"}
+      style={{transform:`translateX(${translation}%)`}}
+    >
+      <div className='absolute -top-[100vh]'/>
+      <span className={`${inter.className} inline-block bg-anti-flash-white p-4 text-xl sm:text-3xl md:text-5xl xl:text-8xl text-charcoal uppercase transition-transform`}>
+        {word}
+      </span>
+    </div>
+  )
+}
+
+export default WordTag
